b0bf29b2579dfdc9f4c05cb4a795b3d34229d42f
kadayogit <guledodowaa@gmail.com>
2024-08-09 23:28:15 +0300

    Adding a nested routes examples

---
+++ src/app/file/page.tsx
@@ -0,0 +1,11 @@
+export default function Profile () {
+    const name = "Guled Mohamed";
+    return(
+        <>
+        <div>
+        <h1>The author: {name}</h1>
+        </div>
+        
+        </>
+    );
+}
