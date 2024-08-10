6d864feea0d638ab01891dda8841860c8ac4cbcb
kadayogit <guledodowaa@gmail.com>
2024-08-10 19:32:12 +0300

    Adding a not found page!

---
+++ src/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+        <div>
+            <h2>Not found Page | 404</h2>
+            <p>could not found this page, please check your URL!</p>
+        </div>
+    );
+}
